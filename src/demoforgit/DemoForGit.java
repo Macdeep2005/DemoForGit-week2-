c266fc7535a08480dba6d764b24f7aba483acad4
Macdeep2005 <memehakd@sheridancollege.ca>
2025-01-17 12:18:11 -0500

    added another comment

--- src/demoforgit/DemoForGit.java
+++ src/demoforgit/DemoForGit.java
@@ -17,6 +17,7 @@ public class DemoForGit {
         // TODO code application logic here
         System.out.println("Hello World!");
 //        adding a comment
+//added another comment
     }
     
 }
