963b540cb25d322093237bb3157b63d267fb0da3
Macdeep2005 <memehakd@sheridancollege.ca>
2025-01-17 12:25:21 -0500

    showing pull

--- src/demoforgit/DemoForGit.java
+++ src/demoforgit/DemoForGit.java
@@ -13,6 +13,7 @@ public class DemoForGit {
     /**
      * @param args the command line arguments
      */
+    // showing pull
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
